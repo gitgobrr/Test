8de9d29b8db40f5f39b31272d696ed297e487f28
Сергей <gitgobrr@gmail.com>
2023-08-12 12:12:03 +0400

    Initial Commit

---
+++ MySparTestTask/MySparTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparTestTaskApp.swift
+//  MySparTestTask
+//
+//  Created by sergey on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySparTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
